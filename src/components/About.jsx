3e5355d74815d2acfd95ee4f9fc5519719147deb
alewonka <epiclimit456@gmail.com>
2025-07-25 15:53:12 +0700

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,19 +39,19 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <p className={styles.sectionSubText}>Pendahuluan</p>
+        <h2 className={styles.sectionHeadText}>Tentang.</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        Saya adalah seorang software developer yang terampil dengan pengalaman dalam TypeScript dan JavaScript, 
+        serta memiliki keahlian pada berbagai framework seperti React, Node.js, dan Three.js. 
+        Saya cepat dalam mempelajari hal baru dan terbiasa bekerja sama secara erat dengan klien untuk menciptakan solusi yang efisien, skalabel, 
+        dan mudah digunakan — guna menyelesaikan masalah nyata.
+        Ayo wujudkan ide-ide Anda bersama saya!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
